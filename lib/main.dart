05355099ee0c61e3ae56059ee21b3009ae5ddcb8
오태현 <jeffrey@kai-i.com>
2022-06-25 17:37:33 +0900

    HomeScreen 구현하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/screen/home/HomeScreen.dart';
+import 'package:video_players/screen/home/HomeScreen.dart';
 
 void main() {
   runApp(
--- lib/screen/home/HomeScreen.dart
+++ lib/screen/home/HomeScreen.dart
@@ -6,13 +6,74 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Home Screen',
-          )
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: getBoxDecoration(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _Logo(),
+            // padding 대신에 SizedBox를 써도 된다
+            SizedBox(
+              height: 30,
+            ),
+            _AppName(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
         ],
       ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/image/logo.png',
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
     );
   }
 }
